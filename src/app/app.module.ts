f597d2c0a438f04bb8f72855c4ef06c91958b82a
ogenefavour <ogenechinweike@gmail.com>
2020-02-16 22:22:50 +0100

    Angular Routing Basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 import 'hammerjs';
 import { AppComponent } from './app.component';
@@ -14,12 +15,22 @@ import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -30,6 +41,7 @@ import { DishService } from './services/dish.service';
     MatCardModule,
     MatButtonModule,
     MatListModule,
+    AppRoutingModule
     
   ],
   providers: [
