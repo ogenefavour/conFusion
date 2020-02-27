543df0ddd6c471655cc9bfc069917cc37bd7eabb
ogenefavour <ogenechinweike@gmail.com>
2020-02-27 09:47:32 +0100

    Reactive Forms Part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { AppRoutingModule } from './app-routing/app-routing.module';
-import{ MatDialogModule } from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
@@ -27,11 +27,12 @@ import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
 
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
 import { LeaderService} from './services/leader.service';
-import { LoginComponent } from './login/login.component'
+
 
 @NgModule({
   declarations: [
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Feedback, ContactType } from '../shared/feedback';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
@@ -12,18 +12,20 @@ export class ContactComponent implements OnInit {
   feedBackForm: FormGroup;
   feedback: Feedback;
   contactType = ContactType;
+  @ViewChild('fform', {static: false}) feedbackFormDirective;
+
   constructor(private fb: FormBuilder) {
     this.createForm();
    }
 
   createForm(){
     this.feedBackForm = this.fb.group({
-      firstname: '',
-      lastname: '',
-      telnum: 0,
-      email: '',
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      telnum: [0, Validators.required],
+      email: ['', Validators.required],
       agree: false,
-      contactType: 'None',
+      contacttype: 'None',
       message: '',
     })
   }
@@ -31,7 +33,16 @@ export class ContactComponent implements OnInit {
   onSubmit(){
     this.feedback = this.feedBackForm.value;
     console.log(this.feedback);
-    this.feedBackForm.reset();
+    this.feedBackForm.reset({
+      firstname: '',
+      lastname: '',
+      telnum: 0,
+      email: '', 
+      agree: false,
+      contactType: 'None',
+      message: '',
+    });
+    this.feedbackFormDirective.resetForm();
   }
   ngOnInit() {
   }
